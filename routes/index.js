a3187ffb3339057e5c12f30c43c03937e94bc46d
Xabi Crespo <xabicrespog@gmail.com>
2015-11-15 13:52:43 +0100

    New projects section

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//var loki = require('lokijs');
+//var db = new loki('projects'); 
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Xabier Crespo' });
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+    res.render('projects', { title: 'Projects' });
 });
 
 module.exports = router;
