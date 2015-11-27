6dabb073cd59494ff21c5b3720c8e7ecd16ea341
Xabi Crespo <xabicrespog@gmail.com>
2015-11-27 11:17:20 +0100

    Routing to project details (no UI yet)

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var projects_db = JSON.parse(fs.readFileSync('routes/projects_db.json', 'utf8'));
 
 //var loki = require('lokijs');
 //var db = new loki('projects'); 
@@ -9,7 +11,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/projects', function(req, res, next) {
-    res.render('projects', { title: 'Projects' });
+    res.render('projects', { title: 'Projects', projects: projects_db });
+});
+
+router.param('project', function (req, res, next, project) {
+  if(projects_db.hasOwnProperty(project))
+  	req.project = projects_db[project];
+  else return next(new Error('I have not collaborated on this project yet. If you think it may fit me, feel free to contact me.'));
+  return next();
+});
+
+router.get('/projects/:project', function(req, res, next) {
+    res.render('project_details', { title: req.project.title, project: req.project });
 });
 
 module.exports = router;
