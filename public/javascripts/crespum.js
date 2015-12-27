3b7f4e1f0f5023bdda6c2b6fe8ba324966744f27
Xabier Crespo Álvarez <xabicrespog@gmail.com>
2015-12-27 17:21:27 +0100

    Light navbar

--- public/javascripts/crespum.js
+++ public/javascripts/crespum.js
@@ -1,9 +1,9 @@
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 70) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-fixed-top").addClass("top-nav-collapse").removeClass("navbar-light");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-fixed-top").removeClass("top-nav-collapse").addClass("navbar-light");
     }
 });
 
